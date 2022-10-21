1128890dce6f76a84ccff397679f2f65f765d5e3
supersuhyeon <suhyeonkr@gmail.com>
2022-10-21 01:25:08 -0700

    responsive swiperjs

--- video/video-js/video.js
+++ video/video-js/video.js
@@ -5,16 +5,43 @@ new Swiper(".intro .mySwiper", {
     },
   });
 
-new Swiper(".gallery-box .swiper-container",{
-  slidesPerView: 3,
-  spaceBetween : 15,
-  loop:true,
-  autoplay: {
-    delay:5000
-  },
-  navigation: {
-    prevEl:'.gallery-box .swiper-prev',
-    nextEl:'.gallery-box .swiper-next'
+
+let ww = window.innerWidth;
+let swiper;
+
+responsiveSwiper()
+
+function initSwiper(slideNum){
+  if(swiper)
+  swiper.destroy();
+
+  return swiper = new Swiper(".gallery-box .swiper-container",{
+    slidesPerView: slideNum,
+    spaceBetween : 15,
+    loop:true,
+    autoplay: {
+      delay:5000
+    },
+    pagination: {
+      el: '.gallery-box .swiper-pagination', //페이지 번호 요소 선택자
+      clickable: true
+    },
+  })
+
+}
+
+function responsiveSwiper(){
+  if(ww >= 1000){
+    initSwiper(3)
+  }else if(ww < 1000 && ww >= 500){
+    initSwiper(2)
+  }else{
+    initSwiper(1)
   }
-  
+}
+
+
+window.addEventListener('resize', ()=>{
+  ww = window.innerWidth;
+  responsiveSwiper()
 })
