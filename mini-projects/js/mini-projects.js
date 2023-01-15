97ff3b200829e3b9471978bc28c93625f5a7ffc3
supersuhyeon <suhyeonkr@gmail.com>
2023-01-15 00:09:20 -0800

    testing

--- mini-projects/js/mini-projects.js
+++ mini-projects/js/mini-projects.js
@@ -1,8 +1,12 @@
-screen.orientation.addEventListener("change", function(e) {
-    if(window.innerWidth >= 1024 && window.innerWidth <= 1900){
-        window.location.href = '/mini-web-project/index.html'
-    }
-});
+let portrait = window.matchMedia("(orientation: portrait)");
+
+portrait.addEventListener("change", function(e) {
+    if(e.matches) {
+        if(window.innerWidth >= 1024 && window.innerWidth <= 1900){
+            window.location.href = '/mini-web-project/index.html'
+        }
+    } 
+})
 
 window.addEventListener('resize', ()=>{
     if(window.innerWidth >= 1024 && window.innerWidth <= 1900){
