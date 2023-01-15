a9b10e95fdd27037f04a4325d71c8347f14ff107
supersuhyeon <suhyeonkr@gmail.com>
2023-01-15 00:01:46 -0800

    testing

--- mini-projects/js/mini-projects.js
+++ mini-projects/js/mini-projects.js
@@ -1,3 +1,8 @@
+screen.orientation.addEventListener("change", function(e) {
+    if(window.innerWidth >= 1024 && window.innerWidth <= 1900){
+        window.location.href = '/mini-web-project/index.html'
+    }
+});
 
 window.addEventListener('resize', ()=>{
     if(window.innerWidth >= 1024 && window.innerWidth <= 1900){
