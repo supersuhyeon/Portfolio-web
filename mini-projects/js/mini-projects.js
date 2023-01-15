a408fabb8735f604144c783c0edf153b7b824c34
supersuhyeon <suhyeonkr@gmail.com>
2023-01-15 00:18:44 -0800

    testing

--- mini-projects/js/mini-projects.js
+++ mini-projects/js/mini-projects.js
@@ -1,12 +1,8 @@
-let landscape = window.matchMedia("(orientation: landscape)");
-
-landscape.addEventListener("change", function(e) {
-    if(e.matches) {
-        if(window.innerWidth >= 1024 && window.innerWidth <= 1900){
-            window.location.href = '/mini-web-project/index.html'
-        }
-    } 
-})
+screen.orientation.onchange = function(e) {
+    if(window.innerWidth >= 1024 && window.innerWidth <= 1900){
+        window.location.href = '/mini-web-project/index.html'
+    }
+ }
 
 window.addEventListener('resize', ()=>{
     if(window.innerWidth >= 1024 && window.innerWidth <= 1900){
